ada962b122e3d4db5973de781bdf414f2ef70e81
Anantha-Rao12 <aanantha.s.rao@gmail.com>
2021-01-04 22:20:41 +0530

    added viterbi decoding

--- HMM.py
+++ HMM.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class HMM_evaluate : 
+class HMM : 
     """
     Given an input Sequence and a HMM ie the set containing Transition probabilitites,
     Emission probabilitites, No of Hidden States and Emitted states, evaluate the probability 
@@ -20,13 +20,11 @@ class HMM_evaluate :
         NHstates = self.N_states
         NEstates = self.N_emitted_states
 
-        state_M = np.zeros((len(NHstates),len(Sequence[0])+1))
-
         # Initialization 
+        state_M = np.zeros((len(NHstates),len(Sequence[0])+1))
         state_M[:,:1] = initial_proba.reshape(len(NHstates),1)
 
         # Forward Algorithm
-
         for i in range(len(Sequence.T)):   
             state_M[:,i+1] = (Tp.T * Ep[:,Sequence.T[i]]) @ state_M[:,i]
         
@@ -38,4 +36,35 @@ class HMM_evaluate :
         computed_M = self.forward_algorithm_matrix(Sequence,initial_proba)
         
         return np.sum(computed_M[:,-1])
+    
+    def vitterbi_algorithm_matrix(self,Sequence,initial_proba):
+        
+        Tp = self.Transition_prob
+        Ep = self.Emission_prob
+        NHstates = self.N_states
+        NEstates = self.N_emitted_states
+        
+        # Initialization 
+        state_M = np.zeros((len(NHstates),len(Sequence[0])+1))
+        state_M[:,:1] = initial_proba.reshape(len(NHstates),1)
+
+        # Forward Algorithm
+        for i in range(len(Sequence.T)): 
+            most_prob_state, max_prob = np.argmax(state_M[:,i]), np.max(state_M[:,i])
+            state_M[:,i+1] = (T[most_prob_state,:]*E[:,most_prob_state])*max_prob            
+        
+        return state_M
+    
+    def decode_sequence(self,Sequence,initial_proba):
+        
+        vitterbi_matrix = self.vitterbi_algorithm_matrix(Sequence,initial_proba)
+        traceback = np.zeros_like(vitterbi_matrix)
+        traceback[vitterbi_matrix.argmax(0),np.arange(len(vitterbi_matrix[0]))] = 1
+        states = np.array(['W'+ str(traceback[:,i].argmax()) for i in range(len(traceback[0]))],dtype=object)
+        
+        states_dict = {'Hidden_states' : states, 'Traceback':traceback}
+        
+        return states_dict
+    
+    
     
